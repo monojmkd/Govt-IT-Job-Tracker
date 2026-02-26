458f7dfb72eba15f9c2fb7e4c8b9768ccd127f49
monojmkd <monoj.mkd@hotmail.com>
2026-02-26 19:35:54 +0530

    tested ai routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const cors = require("cors");
-
+const aiRoutes = require("./routes/ai.routes");
 const jobRoutes = require("./routes/job.routes");
 
 const app = express();
@@ -14,6 +14,7 @@ app.get("/", (req, res) => {
   res.send("Govt Job Tracker API Running");
 });
 
+app.use("/api/ai", aiRoutes);
 app.use("/api/jobs", jobRoutes);
 
 module.exports = app;
